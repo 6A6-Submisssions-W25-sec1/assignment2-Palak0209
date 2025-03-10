653be4b693ac6d7e85f31b14dab2251c040656fe
Palak0209 <2227093@johnabbottcollege.net>
2025-03-10 00:47:21 -0400

    Added public instances of EmailConfig and EmailService class to Appp.xaml.cs

--- MauiEmail/MauiEmail/App.xaml.cs
+++ MauiEmail/MauiEmail/App.xaml.cs
@@ -1,7 +1,13 @@
-﻿namespace MauiEmail
+﻿using EmailConsoleApp.Interfaces;
+using MauiEmail.Configs;
+using MauiEmail.Services;
+
+namespace MauiEmail
 {
     public partial class App : Application
     {
+        public static IMailConfig EmailConfig { get; } = new MailConfig();
+        public static IEmailService EmailService { get; } = new EmailService(EmailConfig);
         public App()
         {
             InitializeComponent();
